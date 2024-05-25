d37feb6b075d1c62da539ccf5c124c0ccf6d641f
Hanbyul Kim <hanstar07@gmail.com>
2024-05-25 09:43:59 +0900

    sitemap 페이지 연결 완료

--- src/components/layout/pcSideMenu.jsx
+++ src/components/layout/pcSideMenu.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useToggleContext } from "../../context/toggleContext";
 import styled, { keyframes } from "styled-components";
+import { Link } from "react-router-dom";
 // react-collapse
 import { Collapse } from "react-collapse";
 
@@ -104,6 +105,20 @@ const MenuDepth2 = styled.ul`
 	}
 `;
 
+const StyledLink = styled(Link)`
+	text-decoration: none;
+
+	color: inherit; /* 부모 요소의 색상을 상속받습니다. */
+
+	&:visited {
+		color: inherit; /* 방문한 링크의 색상도 상속받음 */
+	}
+
+	&:hover {
+		color: #f5954b;
+	}
+`;
+
 const SideMenu = ({ isOpen }) => {
 	const { toggle, toggleValue } = useToggleContext();
 
@@ -117,38 +132,82 @@ const SideMenu = ({ isOpen }) => {
 				<MenuWrapper>
 					<Menu>
 						<MenuTitle>JS의 특별함</MenuTitle>
-						<MenuDepth1>발달 전문의</MenuDepth1>
-						<MenuDepth1>영유아검진 심층 상담</MenuDepth1>
-						<MenuDepth1>정밀 검사 가능</MenuDepth1>
+						<MenuDepth1>
+							<StyledLink onClick={toggleValue} to='/features/1'>
+								발달 전문의
+							</StyledLink>
+						</MenuDepth1>
+						<MenuDepth1>
+							<StyledLink onClick={toggleValue} to='/features/2'>
+								영유아검진 심층 상담
+							</StyledLink>
+						</MenuDepth1>
+						<MenuDepth1>
+							<StyledLink onClick={toggleValue} to='/features/3'>
+								정밀 검사 가능
+							</StyledLink>
+						</MenuDepth1>
 					</Menu>
 					<Menu>
 						<MenuTitle>소아청소년과 진료</MenuTitle>
 						<MenuDepth1
 							onMouseEnter={() => {
-								setCollapseToggle((prev) => [!collapseToggle[0], false, false, false, false]);
+								setCollapseToggle((prev) => [true, false, false, false, false]);
 							}}>
-							일반 진료
+							<StyledLink onClick={toggleValue} to='/clinic/cold'>
+								일반 진료
+							</StyledLink>
 						</MenuDepth1>
 						<Collapse isOpened={collapseToggle[0]}>
 							<MenuDepth2>
-								<li>- 감기</li>
-								<li>- 영유아검진</li>
-								<li>- 예방접종</li>
-								<li>- 수액 치료</li>
+								<li>
+									<StyledLink onClick={toggleValue} to='/clinic/cold'>
+										- 감기
+									</StyledLink>
+								</li>
+								<li>
+									<StyledLink onClick={toggleValue} to='/clinic/infant-check'>
+										- 영유아검진
+									</StyledLink>
+								</li>
+								<li>
+									<StyledLink onClick={toggleValue} to='/clinic/vaccination'>
+										- 예방접종
+									</StyledLink>
+								</li>
+								<li>
+									<StyledLink onClick={toggleValue} to='/clinic/fluid-treatment'>
+										- 수액 치료
+									</StyledLink>
+								</li>
 							</MenuDepth2>
 						</Collapse>
 
 						<MenuDepth1
 							onMouseEnter={() => {
-								setCollapseToggle((prev) => [false, !collapseToggle[1], false, false, false]);
+								setCollapseToggle((prev) => [false, true, false, false, false]);
 							}}>
-							알레르기
+							<StyledLink onClick={toggleValue} to='/clinic/rhinitis'>
+								알레르기
+							</StyledLink>
 						</MenuDepth1>
 						<Collapse isOpened={collapseToggle[1]}>
 							<MenuDepth2>
-								<li>- 비염</li>
-								<li>- 아토피</li>
-								<li>- 천식</li>
+								<li>
+									<StyledLink onClick={toggleValue} to='/clinic/rhinitis'>
+										- 비염
+									</StyledLink>
+								</li>
+								<li>
+									<StyledLink onClick={toggleValue} to='/clinic/atopy'>
+										- 아토피
+									</StyledLink>
+								</li>
+								<li>
+									<StyledLink onClick={toggleValue} to='/clinic/asthma'>
+										- 천식
+									</StyledLink>
+								</li>
 							</MenuDepth2>
 						</Collapse>
 
@@ -156,14 +215,32 @@ const SideMenu = ({ isOpen }) => {
 							onMouseEnter={() => {
 								setCollapseToggle((prev) => [false, false, !collapseToggle[2], false, false]);
 							}}>
-							소아신경
+							<StyledLink onClick={toggleValue} to='/clinic/headache'>
+								소아신경
+							</StyledLink>
 						</MenuDepth1>
 						<Collapse isOpened={collapseToggle[2]}>
 							<MenuDepth2>
-								<li>- 두통</li>
-								<li>- 어지럼증</li>
-								<li>- 경련</li>
-								<li>- 수면</li>
+								<li>
+									<StyledLink onClick={toggleValue} to='/clinic/headache'>
+										- 두통
+									</StyledLink>
+								</li>
+								<li>
+									<StyledLink onClick={toggleValue} to='/clinic/dizziness'>
+										- 어지럼증
+									</StyledLink>
+								</li>
+								<li>
+									<StyledLink onClick={toggleValue} to='/clinic/twitch'>
+										- 경련
+									</StyledLink>
+								</li>
+								<li>
+									<StyledLink onClick={toggleValue} to='/clinic/sleep'>
+										- 수면
+									</StyledLink>
+								</li>
 							</MenuDepth2>
 						</Collapse>
 
@@ -171,12 +248,22 @@ const SideMenu = ({ isOpen }) => {
 							onMouseEnter={() => {
 								setCollapseToggle((prev) => [false, false, false, !collapseToggle[3], false]);
 							}}>
-							성장
+							<StyledLink onClick={toggleValue} to='/clinic/precocious-puberty'>
+								성장
+							</StyledLink>
 						</MenuDepth1>
 						<Collapse isOpened={collapseToggle[3]}>
 							<MenuDepth2>
-								<li>- 성조숙증</li>
-								<li>- 키성장</li>
+								<li>
+									<StyledLink onClick={toggleValue} to='/clinic/precocious-puberty'>
+										- 성조숙증
+									</StyledLink>
+								</li>
+								<li>
+									<StyledLink onClick={toggleValue} to='/clinic/growth-in-height'>
+										- 키성장
+									</StyledLink>
+								</li>
 							</MenuDepth2>
 						</Collapse>
 
@@ -184,38 +271,108 @@ const SideMenu = ({ isOpen }) => {
 							onMouseEnter={() => {
 								setCollapseToggle((prev) => [false, false, false, false, !collapseToggle[3]]);
 							}}>
-							발달지연
+							<StyledLink onClick={toggleValue} to='/clinic/autism'>
+								발달지연
+							</StyledLink>
 						</MenuDepth1>
 						<Collapse isOpened={collapseToggle[4]}>
 							<MenuDepth2>
-								<li>- 자폐</li>
-								<li>- ADHD</li>
-								<li>- 언어지연</li>
+								<li>
+									<StyledLink onClick={toggleValue} to='/clinic/autism'>
+										- 자폐
+									</StyledLink>
+								</li>
+								<li>
+									<StyledLink onClick={toggleValue} to='/clinic/adhd'>
+										- ADHD
+									</StyledLink>
+								</li>
+								<li>
+									<StyledLink onClick={toggleValue} to='/clinic/language-delay'>
+										- 언어지연
+									</StyledLink>
+								</li>
 							</MenuDepth2>
 						</Collapse>
 					</Menu>
 					<Menu>
 						<MenuTitle>소아청소년과 검진</MenuTitle>
-						<MenuDepth1>뇌파 검사</MenuDepth1>
-						<MenuDepth1>자율신경계 검사</MenuDepth1>
-						<MenuDepth1>종합발달심리 검사</MenuDepth1>
-						<MenuDepth1>성장판 검사</MenuDepth1>
-						<MenuDepth1>알레르기 검사</MenuDepth1>
+						<MenuDepth1>
+							<StyledLink onClick={toggleValue} to='/check-up/egg'>
+								뇌파 검사
+							</StyledLink>
+						</MenuDepth1>
+						<MenuDepth1>
+							<StyledLink onClick={toggleValue} to='/check-up/ans'>
+								자율신경계 검사
+							</StyledLink>
+						</MenuDepth1>
+						<MenuDepth1>
+							<StyledLink onClick={toggleValue} to='/check-up/cdpa'>
+								종합발달심리 검사
+							</StyledLink>
+						</MenuDepth1>
+						<MenuDepth1>
+							<StyledLink onClick={toggleValue} to='/check-up/gpa'>
+								성장판 검사
+							</StyledLink>
+						</MenuDepth1>
+						<MenuDepth1>
+							<StyledLink onClick={toggleValue} to='/check-up/at'>
+								알레르기 검사
+							</StyledLink>
+						</MenuDepth1>
 					</Menu>
 					<Menu>
 						<MenuTitle>SPZ아동발달클리닉</MenuTitle>
-						<MenuDepth1>감통치료</MenuDepth1>
-						<MenuDepth1>언어치료</MenuDepth1>
-						<MenuDepth1>ABBA</MenuDepth1>
-						<MenuDepth1>ABBA</MenuDepth1>
+						<MenuDepth1>
+							<StyledLink onClick={toggleValue} to='/cdc/sit'>
+								감통치료
+							</StyledLink>
+						</MenuDepth1>
+						<MenuDepth1>
+							<StyledLink onClick={toggleValue} to='/cdc/st'>
+								언어치료
+							</StyledLink>
+						</MenuDepth1>
+						<MenuDepth1>
+							<StyledLink onClick={toggleValue} to='/cdc/st'>
+								ABBA
+							</StyledLink>
+						</MenuDepth1>
+						<MenuDepth1>
+							<StyledLink onClick={toggleValue} to='/cdc/st'>
+								ABBA
+							</StyledLink>
+						</MenuDepth1>
 					</Menu>
 					<Menu>
 						<MenuTitle>제이에스 소개</MenuTitle>
-						<MenuDepth1>인사말</MenuDepth1>
-						<MenuDepth1>의료진 소개</MenuDepth1>
-						<MenuDepth1>진료시간</MenuDepth1>
-						<MenuDepth1>오시는 길</MenuDepth1>
-						<MenuDepth1>주차장</MenuDepth1>
+						<MenuDepth1>
+							<StyledLink onClick={toggleValue} to='//about-js/greeting'>
+								인사말
+							</StyledLink>
+						</MenuDepth1>
+						<MenuDepth1>
+							<StyledLink onClick={toggleValue} to='//about-js/introduction'>
+								의료진 소개
+							</StyledLink>
+						</MenuDepth1>
+						<MenuDepth1>
+							<StyledLink onClick={toggleValue} to='//about-js/time'>
+								진료시간
+							</StyledLink>
+						</MenuDepth1>
+						<MenuDepth1>
+							<StyledLink onClick={toggleValue} to='//about-js/directions'>
+								오시는 길
+							</StyledLink>
+						</MenuDepth1>
+						<MenuDepth1>
+							<StyledLink onClick={toggleValue} to='//about-js/parking-lot'>
+								주차장
+							</StyledLink>
+						</MenuDepth1>
 					</Menu>
 				</MenuWrapper>
 			</SiteMapWrapper>
